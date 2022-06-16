212465866123973120ea22a61509d2cf83ef31f0
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-16 22:20:27 +0800

    加上session預先設定 Thu Jun 16 10:20:27 PM CST 2022

--- build-push.sh
+++ build-push.sh
@@ -1,6 +1,8 @@
-TAG=php-8-apache-20220525-1748
+#TAG=php-8-apache-20220616-1816
+TAG=php-7.2-apache-20220616-2220
 
 REPO=dlll-paas-base-image
 
 docker build -t pudding/$REPO:$TAG .
 docker push pudding/$REPO:$TAG
+docker rmi pudding/$REPO:$TAG
